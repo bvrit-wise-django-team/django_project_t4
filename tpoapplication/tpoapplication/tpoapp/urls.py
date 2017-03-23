85f903305c7789feb9bd41b5d6e8274e645ec085
siripuram vinitha <vinitha430@gmail.com>
2017-03-23 19:44:38 +0530

    files updated

--- tpoapplication/tpoapplication/tpoapp/urls.py
+++ tpoapplication/tpoapplication/tpoapp/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-
-from . import views
+from .import views
 
 urlpatterns = [
-    url(r'^next/$', views.next, name='next'),
-    url(r'^success/$', views.success, name='success'),
-    url(r'^display/$', views.display, name='display'),
+		
+		url(r'^index/$',views.index,name='index'),
+		url(r'^success/$',views.success,name='success'),
+		url(r'^display/$',views.display,name='display'),
+		
 ]
