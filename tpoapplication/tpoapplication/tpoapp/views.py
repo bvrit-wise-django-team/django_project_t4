806363f1a42b62d5e06589ca1ecfc9ff6899c7a0
siripuram vinitha <vinitha430@gmail.com>
2017-03-23 19:43:18 +0530

    files updated

--- tpoapplication/tpoapplication/tpoapp/views.py
+++ tpoapplication/tpoapplication/tpoapp/views.py
@@ -1,30 +1,32 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect
-from .forms import ViewForm
-from  tpoapp.models import Comment
+from django.shortcuts import render, redirect,render_to_response
+from .forms import DriveForm
+from  TPOv1.models import Drive
 
 
-def next(request):
+def index(request):
     if request.method == "POST":
-        form = ViewForm(request.POST)
-        if form.is_valid():
+        form = DriveForm(request.POST) 
+        if form.is_valid():		
             Drive = form.save(commit = False)
-            Drive.save()
-            
+            Drive.save()            
             return redirect('success')
         
     else:
-        form = ViewForm()
-    return render(request, "next.html", {'form': form})
+        form = DriveForm()
+    return render(request, "index.html", {'form': form})
 
 
 def success(request):
-    return HttpResponse('Success! Notifications Updated.')
-
+			try:
+				drive_details = Drive.objects.all()
+			except Drive.DoesNotExist:
+				raise Http404("DriveDetails does not exist")
+			return render(request, 'viewDrives.html', {'drive_details': drive_details})
+			
 def display(request):
-         try:
-             Drives = Drive.objects.all()
-         except Drive.DoesNotExist:
-             raise Http404("Comment does not exist")
-            
-         return render(request, "display.html",{'Drives': Drives})
+			try:
+				Student = Drive.objects.all()
+			except Drive.DoesNotExist:
+				raise Http404("Comment does not exist")
+			return render(request, "display.html",{'Student': Student})
